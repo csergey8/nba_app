321172225c9ddc8e925f2a36d74ad9651bf9c83f
csergey8 <c.sergey@protonmail.com>
2018-06-11 12:59:58 +0300

    footer component added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link }
+import { Link } from 'react-router-dom';
 import style from  './Footer.css';
+
+import { CURRENT_YEAR } from '../../config';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={style.footer}>
+      <Link to="/" className={style.logo}>
+        <img alt="nba logo" src="/images/nba_logo.png" />
+      </Link>
+      <div className={style.right}>
+        @NBA {CURRENT_YEAR} All rights reserved.
+      </div>
     </div>
   )
 }
