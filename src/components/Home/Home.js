e09d7b932f702ad4da5700d1e8bc6a25dddb932d
csergey8 <c.sergey@protonmail.com>
2018-06-11 17:48:01 +0300

    slider component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NewsSlider from '../widgets/NewsSlider/NewsSlider';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        home
+        <NewsSlider
+          type="featured"
+          start={0}
+          amount={5}
+          settings={{
+            dots: true
+          }}
+        />
       </div>
     )
   }
